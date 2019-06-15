741f023ea3e911cd95840578714a55e8e6bcb635
josue rojas <josuerojas.rojas@gmail.com>
2019-06-15 15:30:04 -0400

    contact form and map created

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -8,4 +8,12 @@ data_file: contact
 
 $(document).ready(function() {
   {% include partial_assets/javascript_partials.js %}
+  $('input[type=text], input[type=email], textarea').focus((e)=>{
+    console.log('focues')
+    $(e.target).closest('.input-wrapper').addClass('focus');
+  });
+  $('input[type=text], input[type=email], textarea').focusout((e)=>{
+    console.log('focues')
+    $(e.target).closest('.input-wrapper').removeClass('focus');
+  });
 });
