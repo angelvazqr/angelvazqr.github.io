75a3026bd5ae01b8d4b87756007fb892ab1ebbbb
josue rojas <josuerojas.rojas@gmail.com>
2019-06-28 21:47:09 -0400

    better home page; no flickering

--- _includes/partial_assets/js/full-background-intro.js
+++ _includes/partial_assets/js/full-background-intro.js
@@ -1,8 +1,9 @@
-if(location.hash == '#fade'){
-  $('.full-background-intro').fadeOut(0);
-
-}
+// if(location.hash == '#fade'){
+//   $('.full-background-intro').fadeOut(0);
+//
+// }
 
 $('.full-background-intro .enter-arrow').click(()=>{
   $('.full-background-intro').fadeOut(400);
+  history.pushState('home', "Home", "/home");
 })
